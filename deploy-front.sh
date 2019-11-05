4e1100b84b9d2af41cfc4f399b7eb3e762ca910a
Tomás Aprile <tomas.eaprile@gmail.com>
2019-11-05 12:28:02 -0300

    better deploy script

--- deploy-front.sh
+++ deploy-front.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 cd .. ; cd frontend
+
+if [ ! -d "./node_modules" ]
+then
+    npm install
+else
+    echo "OK - node_odules exist"
+fi
+
 npm run build
 cd .. ; cd backend
 echo "frontend compiled, now you can run : ./gradlew bootRun"
